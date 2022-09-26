8b7df3b451c6860302b7dddd6524494d960d2533
emilija-smitaite <emilijasmitaite@yahoo.com>
2022-09-26 14:31:20 +0000

    Bug fix: convert float to decimal to allow additon

--- bag/contexts.py
+++ bag/contexts.py
@@ -21,7 +21,7 @@ def bag_contents(request):
         })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
-        delivery = 2.95
+        delivery = Decimal(2.95)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
     else:
         delivery = 0
