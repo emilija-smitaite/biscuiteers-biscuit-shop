3bb4e140ecd645fa16775fe62f107acb7ec65a95
emilija-smitaite <emilijasmitaite@yahoo.com>
2022-10-04 22:21:16 +0000

    Remove max_length attribute in Country fields

--- profiles/migrations/0001_initial.py
+++ profiles/migrations/0001_initial.py
@@ -20,7 +20,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('default_phone_number', models.CharField(blank=True, max_length=20, null=True)),
-                ('default_country', django_countries.fields.CountryField(blank=True, max_length=2, null=True)),
+                ('default_country', django_countries.fields.CountryField(blank=True, null=True)),
                 ('default_postcode', models.CharField(blank=True, max_length=20, null=True)),
                 ('default_town_or_city', models.CharField(blank=True, max_length=40, null=True)),
                 ('default_street_address1', models.CharField(blank=True, max_length=80, null=True)),
