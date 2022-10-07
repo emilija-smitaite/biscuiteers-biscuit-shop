be4398197b9f7737b1c04396bff7d5df45414855
emilija-smitaite <emilijasmitaite@yahoo.com>
2022-10-07 20:37:54 +0000

    Set up AWS

--- biscuiteers/settings.py
+++ biscuiteers/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'blog',
     'profiles',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -173,6 +174,24 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+if 'USE_AWS' in os.environ:
+    # Bucket Config
+    AWS_STORAGE_BUCKET_NAME = 'biscuiteers-biscuit-shop'
+    AWS_S3_REGION_NAME = 'us-east-1'
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+    # Static and media files
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_LOCATION = 'static'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    MEDIAFILES_LOCATION = 'media'
+
+    # Override static and media URLs in production
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
+
 # Stripe
 FREE_DELIVERY_THRESHOLD = 25
 STANDARD_DELIVERY_PERCENTAGE = 10
@@ -181,5 +200,3 @@ STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 DEFAULT_FROM_EMAIL = 'biscuiters@example.com'
-
-STATIC_ROOT = BASE_DIR / "staticfiles"
